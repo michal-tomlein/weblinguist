9a058efb81c55ae4aecdc191c5e17ff464227da6
michal.tomlein@gmail.com <michal.tomlein@gmail.com@6a720c97-dc4e-0410-9872-05944308408f>
2010-10-06 16:58:24 +0000

    Weblinguist 0.1.1 RC1

--- trunk/about_widget.cpp
+++ trunk/about_widget.cpp
@@ -31,11 +31,11 @@ AboutWidget::AboutWidget(QString ver, QString year)
     about.append(tr("Weblinguist is a Qt web development tool which allows you to translate web pages with ease. It features an intuitive user interface similar to Qt Linguist."));
     about.append("</p><p></p>");
 	about.append("<p style=\"font-family: sans-serif; font-size:8pt;\">");
-    about.append(tr("This programme is distributed under the terms of the GPL v2."));
+    about.append(tr("This program is distributed under the terms of the GPL v2."));
 	about.append("</p><p></p>");
 	about.append(QString("<p style=\"font-family: sans-serif; font-size:8pt;\">%1 <span style=\"font-style:italic;\">Michal Tomlein</span> (michal.tomlein@gmail.com)</p>").arg(year));
 	about.append("<p></p><p style=\"font-family: sans-serif; font-size:8pt;\">");
-	about.append(tr("The programme is provided AS IS with ABSOLUTELY NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE."));
+	about.append(tr("The program is provided AS IS with ABSOLUTELY NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE."));
 	about.append("</p>");
 #ifdef Q_WS_MAC
 	about.remove("font-family: sans-serif;");
@@ -46,7 +46,7 @@ AboutWidget::AboutWidget(QString ver, QString year)
 	QString about_qt = "<p style=\"font-family: sans-serif; font-style:italic;\"><span style=\"font-size:12pt;\">";
     about_qt.append(tr("About Qt"));
 	about_qt.append("</span></p><p></p><p style=\"font-family: sans-serif; font-size:8pt; font-style:italic;\">");
-	about_qt.append(tr("This programme uses Qt Open Source Edition version %1.").arg(qVersion()));
+	about_qt.append(tr("This program uses Qt Open Source Edition version %1.").arg(qVersion()));
 	about_qt.append("</p><p></p><p style=\"font-family: sans-serif; font-size:8pt;\">");
 	about_qt.append(tr("Qt is a C++ toolkit for cross-platform application development."));
 	about_qt.append("</p><p></p><p style=\"font-family: sans-serif; font-size:8pt;\">");
